4397c964512762aefbbae95f45ee040789439148
Lenny T <git@sweetleon.com>
2015-12-27 05:05:15 -0800

    fix compilation issues

--- Romo/lib/Drive/RomoAV/RAVVideoInput.m
+++ Romo/lib/Drive/RomoAV/RAVVideoInput.m
@@ -24,6 +24,8 @@
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
+#define SIMULATOR TARGET_IPHONE_SIMULATOR
+
 @interface RAVVideoInput () {
     AVCaptureSession            *_captureSession;
     AVCaptureDevice             *_captureDevice;
